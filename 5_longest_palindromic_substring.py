5110c2a718f06f2db3788280d649af42a3e637c2
waynewu6250 <waynewu6250@gmail.com>
2019-07-26 21:54:33 +0800

    7/26

---
+++ 5_longest_palindromic_substring.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        n = len(s)
+        dp = [[0]*n for _ in range(n)]
+        
+        start = 0
+        maxlen = 1
+        
+        # initialization
+        for i in range(n):
+            dp[i][i] = 1
+            if i < n-1 and (s[i] == s[i+1]):
+                dp[i][i+1] = 1
+                start = i
+                maxlen = 2
+        
+        # run
+        for i in range(n-1,-1,-1):
+            for j in range(i+2,n):
+                if dp[i+1][j-1] and (s[i] == s[j]):
+                    dp[i][j] = 1
+                    if (j-i >= maxlen):
+                        start = i
+                        maxlen = j-i+1
+        
+        return s[start:start+maxlen]
