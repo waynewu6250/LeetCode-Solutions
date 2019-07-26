a1554df4e9cb67aee91fc43780244b29be83d466
waynewu6250 <waynewu6250@gmail.com>
2019-07-26 21:55:07 +0800

    7/26

---
+++ 746_min_cost_climbing_stairs.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        n = len(cost)
+        
+        for i in range(2, n):
+            cost[i] = cost[i]+min(cost[i-1], cost[i-2])
+        return min(cost[n-1],cost[n-2])
