a03fdf33e449dffb36578fead5fd5a08ee4f309a
waynewu6250 <waynewu6250@gmail.com>
2019-07-28 00:06:02 +0800

    7/28

---
+++ 289.game-of-life.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=289 lang=python3
+#
+# [289] Game of Life
+#
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        
+
--- 48.rotate-image.py
+++ 48.rotate-image.py
@@ -8,5 +8,16 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+
+        n = len(matrix)
+        for i in range(n//2):
+            for j in range(i,n-i-1):
+                tmp = matrix[i][j]
+                matrix[i][j] = matrix[n-j-1][i]
+                matrix[n-j-1][i] = matrix[n-i-1][n-j-1]
+                matrix[n-i-1][n-j-1] = matrix[j][n-i-1]
+                matrix[j][n-i-1] = tmp
+
+
         
 
