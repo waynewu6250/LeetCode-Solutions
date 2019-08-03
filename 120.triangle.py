f30e06566e6ff5ca307caba52046318b3230607c
waynewu6250 <waynewu6250@gmail.com>
2019-08-03 17:36:56 -0400

    8/2

---
+++ 120.triangle.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=120 lang=python3
+#
+# [120] Triangle
+#
+class Solution:
+    
+    def minimumTotal3(self, triangle):
+        if not triangle:
+            return 
+        for i in range(len(triangle)-2, -1, -1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+        return triangle[0][0]
+
+    # bottom-up, O(n) space
+    def minimumTotal(self, triangle):
+        if not triangle:
+            return 
+        res = triangle[-1]
+        for i in range(len(triangle)-2, -1, -1):
+            for j in range(len(triangle[i])):
+                res[j] = min(res[j], res[j+1]) + triangle[i][j]
+        return res[0]
+        
+
