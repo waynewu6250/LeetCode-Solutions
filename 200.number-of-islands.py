091fe70ed1fb2dcd497d8478b1afb4e7900da34a
waynewu6250 <waynewu6250@gmail.com>
2020-02-12 20:35:29 -0500

    add

--- 200.number-of-islands.py
+++ 200.number-of-islands.py
@@ -4,65 +4,53 @@
 # [200] Number of Islands
 #
 class Solution:
-    # dfs method
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        m = len(grid)
-        if m == 0: return 0
-        n = len(grid[0])
         
-        ans = 0
-        for y in range(m):
-            for x in range(n):
-                if grid[y][x] == '1':
-                    ans += 1
-                    self.__dfs(grid, x, y, n, m)
-        return ans
-    
-    def __dfs(self, grid, x, y, n, m):
-        if x < 0 or y < 0 or x >=n or y >= m or grid[y][x] == '0':
-            return
-        grid[y][x] = '0'
-        self.__dfs(grid, x + 1, y, n, m)
-        self.__dfs(grid, x - 1, y, n, m)
-        self.__dfs(grid, x, y + 1, n, m)
-        self.__dfs(grid, x, y - 1, n, m)
-    
-
-    #################################################
-    # bfs method (recommended)
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
+        # 1. '1'
+        # 2. visited = (grid[x][y] == 0)
         
         if not grid or not grid[0]:
             return 0
         
-        islands = 0
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
-                if grid[x][y] == '1':
-                    self.bfs(grid, x, y)
-                    islands += 1
-        return islands
-    
-    
-    def bfs(self, grid, x, y):
+        ans = 0
         
-        queue = collections.deque([(x,y)])
-        grid[x][y] = '0'
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    ans += 1
+                    #self.bfs(grid, i, j)
+                    self.dfs(grid, i, j)
+        return ans
+    
+    # dfs approach
+    def dfs(self, grid, i, j):
+        
+        if not (0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == '1'):
+            return
+        
+        grid[i][j] = '0'
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
+
+    
+    # bfs approach
+    def bfs(self, grid, i, j):
+        
+        queue = collections.deque([(i,j)])
+        grid[i][j] = '0'
         
         while queue:
-            x, y = queue.popleft()
-            for x_inc, y_inc in [(1,0), (0,1), (-1,0), (0,-1)]:
+            (x, y) = queue.popleft()
+            
+            for x_inc, y_inc in [(0,1), (0,-1), (1,0), (-1,0)]:
                 x_new = x + x_inc
                 y_new = y + y_inc
-                if not (0 <= x_new < len(grid) and 0 <= y_new < len(grid[0]) and grid[x_new][y_new] == '1'):
-                    continue
-                queue.append((x_new, y_new))
-                grid[x_new][y_new] = '0'
+                
+                if 0 <= x_new < len(grid) and 0 <= y_new < len(grid[0]) and grid[x_new][y_new] == '1':
+                    queue.append((x_new, y_new))
+                    grid[x_new][y_new] = '0'
 
         
 
