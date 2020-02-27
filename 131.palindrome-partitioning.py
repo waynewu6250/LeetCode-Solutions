e1fe280c434d3088461547f7adbf363b78202e41
waynewu6250 <waynewu6250@gmail.com>
2020-02-26 21:51:39 -0500

    add

--- 131.palindrome-partitioning.py
+++ 131.palindrome-partitioning.py
@@ -30,4 +30,36 @@ class Solution:
         recur(items, 0)
         return answer
         
-
+######################################################
+class Solution(object):
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        
+        answer = []
+        items = []
+        self.dfs(answer, items, 0, s)
+        return answer
+    
+    def dfs(self, answer, items, start, s):
+        
+        print(start)
+        
+        if start == len(s):
+            answer.append(items)
+        
+        for i in range(start, len(s)):
+            if self.is_palindrome(s, start, i):
+                self.dfs(answer, items + [s[start:i+1]], i+1, s)
+            
+    
+    def is_palindrome(self, s, i, j):
+        
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
