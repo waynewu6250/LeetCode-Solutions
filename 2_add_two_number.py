3c704efa03954ebaa4ba99460a8509f5840f4f1b
waynewu6250 <waynewu6250@gmail.com>
2020-03-09 17:01:26 -0400

    add

--- 2_add_two_number.py
+++ 2_add_two_number.py
@@ -4,6 +4,7 @@ class ListNode:
         self.val = x
         self.next = None
 
+# Recursive method
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode: 
         
@@ -27,3 +28,43 @@ class Solution:
             return result
         
         return add(l1,l2,0)
+
+
+# Iterative method
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        
+        l1_ptr = l1
+        l2_ptr = l2
+        dummy = ListNode(-1)
+        new_ptr = dummy
+        
+        carry = 0
+        while l1_ptr or l2_ptr:
+            
+            x = l1_ptr.val if l1_ptr else 0
+            y = l2_ptr.val if l2_ptr else 0
+            
+            s = (x + y + carry) % 10
+            
+            new_node = ListNode(s)
+            new_ptr.next = new_node
+            new_ptr = new_node
+            
+            carry = (x + y + carry) // 10
+            
+            if l1_ptr:
+                l1_ptr = l1_ptr.next
+            if l2_ptr:
+                l2_ptr = l2_ptr.next
+        
+        if carry > 0:
+            new_ptr.next = ListNode(carry)
+        
+        
+        return dummy.next
